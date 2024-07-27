bf1b6ab5be4e3c69d225ae9048ae1aa2f63e3514
Alex <alex@lego>
2024-07-27 16:24:42 +0300

    update room id

--- common.py
+++ common.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 CREDS_FILE = (Path(os.path.dirname(os.path.realpath(__file__))) / "credentials.json").resolve()
 HOMESERVER = "https://matrix.altau.su"
-USER_ID = "@DiscordWatcherBot:altau.su"
+USER_ID = "@discordwatcherbot:altau.su"
 DEVICE_NAME = "lego"
-ROOM_ID = "!YGpyzzzDSofdTiLCZg:altau.su"
+ROOM_ID = "!UHGnnNBOaHpZbAPrbe:altau.su"
